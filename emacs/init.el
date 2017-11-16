2ce630971518df206092e6cd44a5aa980d9c11d5
aburdulescu <andrei.burdulescu@protonmail.com>
2017-11-16 17:59:05 +0200

    Move configs to init.el file instead of .emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,8 +1,8 @@
-;;=========Import MELPA=======================
 (require 'package)
 
 (add-to-list 'package-archives '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-(add-to-list 'load-path "~/repos/configs/emacs/")
+# add custom configs
+(add-to-list 'load-path "/c/Users/User/repos/configs/emacs/")
 (require 'my_config )
