79c754469b507bae0b969d0b4934c5dd4513923f
aburdulescu <andrei.burdulescu@protonmail.com>
2017-12-24 08:02:47 -0500

    Remove C-x-k helm kbd

--- emacs/cfgs/helm-cfg.el
+++ emacs/cfgs/helm-cfg.el
@@ -7,5 +7,5 @@
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 (global-set-key (kbd "C-x C-b") #'helm-buffers-list)
 (global-set-key (kbd "C-x b") #'helm-buffers-list)
-(global-set-key (kbd "C-x k") #'helm-buffers-list)
+;(global-set-key (kbd "C-x k") #'helm-buffers-list)
 (helm-mode 1)
