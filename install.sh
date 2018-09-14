fc8c1515f091e5ab4b3237eea32751c5c781713a
aburdulescu <andrei.burdulescu@protonmail.com>
2018-09-14 08:30:52 -0400

    More modifications fow stow

--- install.sh
+++ install.sh
@@ -2,31 +2,35 @@
 
 cleanup()
 {
+    rm -rf ~/.emacs.d \
+       ~/.bash_aliases \
+       ~/.zsh_aliases \
+       ~/.tmux.conf
 }
 
 install_packages()
 {
-    PACKAGES="git \
-              emacs \
-              build-essential \
-              tmux \
-              cmake \
-              stow"
-
     sudo apt-get update
-    sudo apt-get install $PACKAGES
+    sudo apt-get install -y git \
+         emacs \
+         build-essential \
+         tmux \
+         cmake \
+         stow
 }
 
 install_dotfiles()
 {
     stow tmux
-    [[ "$SHELL" == "/usr/bin/zsh" ]] && stow zsh || [[ "$SHELL" == "/bin/bash" ]] && stow bash
+    [[ "$SHELL" == "/usr/bin/zsh" ]] && stow zsh && echo ". ~/.zsh_aliases" >> ~/.zshrc
+    [[ "$SHELL" == "/bin/bash" ]] && stow bash
     stow emacs
 }
 
 main()
 {
     install_packages
+    cleanup
     install_dotfiles
 }
 
