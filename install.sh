02dc44700f53f59e041d25596bcfe1f69adc3cf4
aburdulescu <andrei.burdulescu@protonmail.com>
2018-10-10 08:33:30 -0400

    Add more packages

--- install.sh
+++ install.sh
@@ -11,13 +11,21 @@ cleanup()
 
 install_packages()
 {
-    sudo apt-get update
-    sudo apt-get install -y git \
-         emacs \
+    sudo apt update
+    sudo apt install -y \
+         xorg \
+         i3 \
+         lightdm \
+         module-assistant \
          build-essential \
          tmux \
          cmake \
+         zsh \
+         python3 \
+         python3-pip \
+         python3-venv \
          stow
+    sudo apt build-dep emacs     
 }
 
 install_dotfiles()
@@ -31,12 +39,22 @@ install_dotfiles()
 
 main()
 {
+    set -e # stop on error
+    set -x # print what's executed
+
     install_packages
     [[ "$1" == "-f" ]] && cleanup
     install_dotfiles
+
+    set +e # stop on error
+    set +x # print what's executed
+
+    echo -e "Now you have to:\n"
+    echo -e "\tInstall oh-my-zsh:"
+    echo 'sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+
+    echo -e "\n\tInstall emacs:"
+    echo "Download emacs source from ftp and install it!!"
 }
 
-
-set -e # stop on error
-set -x # print what's executed
 main $@
